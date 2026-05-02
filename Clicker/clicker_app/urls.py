76d2863a9e1451d42539bc3ef65bea3934f6a617
Stomy <stomy4073@gmail.com>
2026-05-02 15:45:31 +0300

    Initial commit from local project

---
+++ Clicker/clicker_app/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterView, ProfileView
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('shop/', views.my_api_page, name='api_shop'),
+    path('moreclicks-btn/', views.buy_upgrade, name='buy_upgrade'),
+]
+
+
+
+
+
+
+
+
---
+++ Clicker/clicker_app/views.py
@@ -0,0 +1,46 @@
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from .serializers import UserSerializer, ProfileSerializer
+from .models import Profile
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+class ProfileView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        profile = request.user.profile
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    def post(self, request):
+        profile = request.user.profile
+        profile.clicks += profile.click_power
+        profile.save()
+        return Response({'clicks': profile.clicks})
+
+
+@api_view(['GET'])
+def my_api_page(request):
+    data = {
+        'message': 'Привет! Это данные с новой страницы API',
+        'status': 'success'
+    }
+    return Response(data)
+
+@api_view(['POST'])
+def buy_upgrade(request):
+    profile = request.user.profile
+    if profile.clicks >= 100: # Цена улучшения
+        profile.clicks -= 100
+        profile.click_power += 1 # Теперь клик дает 2
+        profile.save()
+        return Response({'success': True})
+    return Response({'error': 'Недостаточно кликов'}, status=400)
